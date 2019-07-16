3e4072d3ba33d8e929a2adb7f6feb74d2cf33492
sandykrishdaswani <51950647+sandykrishdaswani@users.noreply.github.com>
2019-07-16 16:32:53 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -4,7 +4,7 @@ if(len(sandy1)>len(uma1)):
   sandy1,uma1=uma1,sandy1
 z1=0
 while(z1<len(sandy1)):
-  y1+=(ord(b1[z1])-ord(a1[z1]))
+  y1+=(ord(uma1[z1])-ord(sandy1[z1]))
   z1+=1
 for z1 in range(z1,len(uma1)):
   y1+=ord(b1[z1])-ord('sandy')+1
