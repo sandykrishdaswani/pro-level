42db3bf92592d909b7e38d7ee3d57d6cdbd2f657
sandykrishdaswani <51950647+sandykrishdaswani@users.noreply.github.com>
2019-07-24 23:13:18 +0530

    Update 16.py

--- 16.py
+++ 16.py
@@ -6,6 +6,6 @@ for i in range(sandy1):
         if cat[i]>cat[i+1]:
             y[i]=y[i]+y[i+1]
     elif i>0:
-        if cat[i]>c[i-1]:
+        if cat[i]>cat[i-1]:
             y[i]=y[i]+y[i-1]
 print(sum(y))
