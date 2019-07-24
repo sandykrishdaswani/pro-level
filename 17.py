4862c7a75dd526ca36efa759b4e77bf6a3dd5993
sandykrishdaswani <51950647+sandykrishdaswani@users.noreply.github.com>
2019-07-24 23:21:46 +0530

    Create 17.py

---
+++ 17.py
@@ -0,0 +1,8 @@
+sandy, krish = map(int, input().split())
+x = list(map(int, input().split()))
+for i in permutations(x, 2):
+    if sum(i) == krish:
+        print('yes')
+        break
+else:
+    print('no')
