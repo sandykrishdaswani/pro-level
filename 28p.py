3eb50f2b4f6795f8cd05c1da2cc66dda47ddf4f9
sandykrishdaswani <51950647+sandykrishdaswani@users.noreply.github.com>
2019-08-09 15:16:15 +0530

    Create 28p.py

---
+++ 28p.py
@@ -0,0 +1,10 @@
+sandy1=int(input())
+mom1=list(map(int,input().split()))
+mom1.sort()
+sin1=0
+san1=0
+for i in range(len(mom1)):
+    if mom1[i]>=sin:
+        san1=san1+1
+    sin1=sin1mom1[i]
+print(san1)
