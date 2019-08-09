ca48adacbe58f281f453b05bbdabcc56c629955b
sandykrishdaswani <51950647+sandykrishdaswani@users.noreply.github.com>
2019-08-09 15:30:29 +0530

    Create 29.py

---
+++ 29.py
@@ -0,0 +1,25 @@
+sandy=int(input())
+isi=0
+xsy=0
+bs=[]
+while isi<90 and ii<inp:
+
+  ss=0
+
+  for j in str(sandy-isi):
+
+    ss+=int(j)
+
+  if ss+(sandy-isi)==sandy:
+
+    xy+=1
+
+    bs.append(sandy-isi)
+
+  isi+=1
+
+print(xsy)
+
+for isi in bs:
+
+  print(isi)
